a80678148c569c8ec01cb27f431fe6fbebcac8e7
Hongyin Wang <yusufwhy@gmail.com>
2024-06-12 16:35:53 +0200

    Second commit: mejora en la coneción con el principal

--- agencia_de_viaje.py
+++ agencia_de_viaje.py
@@ -1,31 +1,29 @@
 # Tres partes: 1. menú principal y reserva ; 2. cliente relacionado; 3. destino relacionado
 # Parte Hongyin - funciones con destinos relacionado
 # Añadir un nuevo destino (Código destino, Nombre destino y Precio destino)
-# def addDestinatoin(listaDestino):
+def addDestino(listaDestino):
 # add ese código al inicio del principal: listaDestino = []
-listaDestino = []
-for i in range(4):
-        codigoDestino = input('Introduzca el código del destino nuevo, for favor.\n')
-        nombreDestino = input('Introduzca el nombre de ese destino nuevo, por favor.\n')
-        while True:
-            try:
-                precioDestino = float(input('Introduzca el precio para ese destino nuevo, por favor.\n'))
-                break
-            except ValueError:
-                print('El precio no puede ser otro formato fuera de ser numeros.')
-# from modulos.screen import pause_screen, clear_screen (Domingo´s modulos)
-        nuevoDestino = {
-            'codigo' : codigoDestino,
-            'nombre' : nombreDestino,
-            'precio' : precioDestino, 
-            }
-        print(f'El nuevo destino {nombreDestino} con código {codigoDestino} se ha añadido con éxito y su precio será €{precioDestino}.')
-        listaDestino.append(nuevoDestino)
-print(listaDestino)
-#def showDestination(listaDestino):
-for j, destinos in enumerate(listaDestino):
-     print(f'n\ Destino {j + 1}')
-     for key, value in destinos.items():
-          print(f'\t {key} \t {value}')
-     
-     
+# 1º añadir el destino
+    codigoDestino = input('Introduzca el código del destino nuevo, for favor.\n')
+    nombreDestino = input('Introduzca el nombre de ese destino nuevo, por favor.\n')
+    while True:
+        try:
+            precioDestino = float(input('Introduzca el precio para ese destino nuevo, por favor.\n'))
+            break
+        except ValueError:
+            print('El precio no puede ser otro formato fuera de ser numeros.')
+    # from modulos.screen import pause_screen, clear_screen (Domingo´s modulos)
+    nuevoDestino = {
+        'codigo' : codigoDestino,
+        'nombre' : nombreDestino,
+        'precio' : precioDestino, 
+        }
+    print(f'El nuevo destino {nombreDestino} con código {codigoDestino} se ha añadido con éxito y su precio será €{precioDestino}.')
+    listaDestino.append(nuevoDestino)
+    print(listaDestino)
+def showDestino(listaDestino):
+# add ese código al inicio del principal: listaDestino = []
+    for j, destinos in enumerate(listaDestino):
+        print(f'\nDestino {j + 1}')
+        for key, value in destinos.items():
+            print(f'{key} \t {value}')
