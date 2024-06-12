97f196eda81b78d76c0fc3b4cefdd65b13a69d3d
kike-l <172401006+kike-l@users.noreply.github.com>
2024-06-12 17:22:51 +0200

    commit de kike numer 2

--- principal.py
+++ principal.py
@@ -1,27 +1,16 @@
-#principal
-"""PythonAC1 - Projecte Agència de Viatges
-Vamos a crear un programa para una agencia de viajes que permita gestionar destinos, clientes y reservas. Este programa tendrá como mínimo las siguientes funcionalidades:
-Añadir un nuevo destino (Código destino, Nombre destino y Precio destino).
-Añadir un nuevo cliente (ID cliente y Nombre cliente.
-Realizar una reserva (Pasaremos un código de destino y un ID de cliente).
-Cancelar una reserva (Pasaremos un código de destino y un ID de cliente).
-Mostrar todos los destinos (Mostraremos código, nombre y precio).
-Mostrar todos los clientes (Mostraremos ID y nombre).
-Mostrar todas las reservas (Mostraremos Cliente y Reserva).
-Nuestro programa tiene que cumplir:
-Los parámetros entre paréntesis son orientativos y pueden ayudar a la funcionalidad del programa, podemos decidir otros.
-Las entradas y salidas de datos tienen que ser amigables con el usuario. O sea, darle un formato adecuado.
-Debemos controlar que las entradas de datos cumplan ciertos parámetros y crear las excepciones necesarias.
-Como mínimo deberíamos de tener dos módulos.
-Todas las funciones tienen que tener su correspondiente documentación (docstring), con su funcionamiento, parámetros de entrada y parámetros de salida.
-Uso de Git y GitHub para el control de versiones. Debéis añadirme como colaborador al repositorio de GitHub, donde también se realizará la entrega.
-Formato de entrega: Enlace al repositorio de GitHub (1 por grupo).
-Componentes de los grupos:
-Grupo 1: Domingo, Hongyin y Enrique.
-Grupo 2: Daniel, Alex y Jesús.
-Grupo 3: Francisco, Volodymyr y María.
-Grupo 4: Carlos, Albert y Caleb.
-Fecha de entrega: 13 jun"""
+#imports
+import os
+from pakages.gestion_reservas.add_reserva import reservar, cancelar_Reserva
+from pakages.gestion_costumers.cliente import 
+from pakages.gestion_destino.destino import 
+from pakages.gestion_mostrar.mostrar import 
+def clearP():
+    os.system('cls')
+
+def pausaP():
+    print('enter para seguir')
+    input()
+
 
 def principal():
 
@@ -42,36 +31,48 @@ def principal():
         try:
             sel = int(input('Introduzca una seleccion del menu:'))
         except ValueError:
-            print('Por favor haga la seleccion de forma numerica\n Tipo---> 1 (para seleccionar Añadir un nuevo destino) ')
+            clearP()
+            print('\nPor favor haga la seleccion de forma numerica\n\t\t Tipo---> 1 (para seleccionar Añadir un nuevo destino) ')
         else:
             if 8 <= sel < 1: 
                 match sel:
                     case 1:
-                        print('Selecciono Añadir un nuevo destino')
-                        nDestino()
+                        clearP()
+                        print('\n\t\tSelecciono Añadir un nuevo destino')
+                        
                     case 2:
-                        print('Selecciono Añadir un nuevo cliente ')
-                        nCliente()
+                        clearP()
+                        print('\n\t\tSelecciono Añadir un nuevo cliente ')
+                        
                     case 3:
-                        print('Selecciono Realizar una reserva ')
-                        nReservas()
+                        clearP()
+                        print('\n\t\tSelecciono Realizar una reserva ')
+                        reservar(agencia)
+                        
                     case 4:
-                        print('Selecciono Cancelar una reserva ')
-                        cReservas()
+                        clearP()
+                        print('\n\t\tSelecciono Cancelar una reserva ')
+                        cancelar_Reserva(agencia)
                     case 5:
-                        print('Selecciono  Mostrar todos los destinos ')
-                        mDestinos()
+                        clearP()
+                        print('\n\t\tSelecciono  Mostrar todos los destinos ')
+                        
                     case 6:
-                        print('Selecciono  Mostrar todos los clientes ')
-                        mClientes()
+                        clearP()
+                        print('\n\t\tSelecciono  Mostrar todos los clientes ')
+                        
                     case 7:
-                        print('Selecciono  Mostrar todos los reservas ')
-                        mReservas()
+                        clearP()
+                        print('\n\t\tSelecciono  Mostrar todos los reservas ')
+                        
                     case 8:
-                        print('Selecciono  Salir del Programa ')
+                        clearP()
+                        print('\n\t\tSelecciono  Salir del Programa ')
                         flagMen = False
                     case _:
-                        print('Algo salio muy mal saliendo del Programa...')
+                        clearP()
+                        print('\n\t\tAlgo salio muy mal saliendo del Programa...')
                         break
             else:
-                print('La seleccion debe estar en el menu es decir entre el 1 y el 8')
+                clearP()
+                print('\n\t\tLa seleccion debe estar en el menu es decir entre el 1 y el 8')
