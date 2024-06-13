020a76cb77b574a47a18bceb4ce9d8341c7270b3
Hongyin Wang <yusufwhy@gmail.com>
2024-06-13 17:55:45 +0200

    HW: unos cambio menor después de la última revision.

--- modulos/anadir_cliente.py
+++ modulos/anadir_cliente.py
@@ -11,33 +11,25 @@ def anadir_cliente(clientes):
             - Lista de clientes actualizada
     '''
     index = len(clientes)
-
     while True:
         if clientes:
             print(f'Tenemos clientes en nuestra base de datos\n')
             mc(clientes)
         else:
             print(f'No hay clientes en nuestra base de datos\n')
-
-        print(f'A continuación vas a introducir un nuevo cliente a nuestra base de datos.\n Necesitarás:\n \t-Nombre del cliente')
+        print(f'A continuación vas a introducir un nuevo cliente a nuestra base de datos.\n Se recomendará introducir ambos el nombre y el apellido del cliente\n')
         
-
         while True:
             nombre_cliente = input('Introduce el nombre del cliente').lower()
-
             if not nombre_cliente:
                 print(f'\nNo has introducido un nombre de cliente')
-            
             elif not nombre_cliente.isdigit():
                 index += 1
                 id_cliente = str(index) + ''.join([word[0] for word in nombre_cliente.split()])
                 break
             else:
-                print('El nombre del cliente no puede ser de números sólo: \t')
-
-
+                print('El nombre del cliente no puede ser de números sólo.')
         clientes.append({'codigo destino': '', 'nombre destino': '', 'precio destino': '', 'reserva': False, 'codigo destino': '', 'id cliente': id_cliente, 'nombre cliente': nombre_cliente })
-        
         print(f'El nuevo cliente {nombre_cliente} con ID {id_cliente} se ha añadido con éxito.')
         
         epilogue = input('Para seguir introduciendo clientes presiona enter. Si no, puedes presionar * para volverte al menu principal. \n')
